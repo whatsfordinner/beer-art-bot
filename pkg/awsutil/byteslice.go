1c5b698983434399769ca6cfa116c803a7378daf
whatsfordinner <cruelangeleo7@gmail.com>
2019-10-19 22:50:13 +1100

    exporting awsutil functions

--- pkg/awsutil/byteslice.go
+++ pkg/awsutil/byteslice.go
@@ -10,7 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-func writeByteSliceToS3(bucket string, key string, blob []byte, sess *session.Session) error {
+func WriteByteSliceToS3(bucket string, key string, blob []byte, sess *session.Session) error {
 	uploader := s3manager.NewUploader(sess)
 	log.Printf("uploading %s to %s", key, bucket)
 	result, err := uploader.Upload(&s3manager.UploadInput{
@@ -25,7 +25,7 @@ func writeByteSliceToS3(bucket string, key string, blob []byte, sess *session.Se
 	return nil
 }
 
-func getByteSliceFromS3(bucket string, key string, sess *session.Session) ([]byte, error) {
+func GetByteSliceFromS3(bucket string, key string, sess *session.Session) ([]byte, error) {
 	downloader := s3manager.NewDownloader(sess)
 	awsBuff := aws.NewWriteAtBuffer(make([]byte, 0))
 	_, err := downloader.Download(awsBuff, &s3.GetObjectInput{
