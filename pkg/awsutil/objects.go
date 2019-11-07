f33d9d1c9a0df16ef83c99e68d73a4ab751c6a1d
whatsfordinner <cruelangeleo7@gmail.com>
2019-11-07 22:29:36 +1100

    added utility to check if object exists in a bucket

---
+++ pkg/awsutil/objects.go
@@ -0,0 +1,30 @@
+package awsutil
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// ObjectExistsInBucket returns true if the key provided matches a single object in the bucket
+// provided or false otherwise
+func ObjectExistsInBucket(bucket string, key string, sess *session.Session) (bool, error) {
+	svc := s3.New(sess)
+	input := &s3.ListObjectsInput{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String(key),
+	}
+
+	result, err := svc.ListObjects(input)
+	if err != nil {
+		return false, err
+	}
+
+	for _, object := range result.Contents {
+		if *object.Key == key {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
