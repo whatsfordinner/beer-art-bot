d21cca2bcdcde2cdb04cb9777a64267f161a20da
whatsfordinner <cruelangeleo7@gmail.com>
2019-11-16 13:52:07 +1100

    refactored getbeers to use new functions in brewerydb package and move config collection into its own function

--- cmd/getbeers/getbeers.go
+++ cmd/getbeers/getbeers.go
@@ -2,61 +2,65 @@
 package main
 
 import (
-	"encoding/json"
+	"errors"
 	"log"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/whatsfordinner/beer-art-bot/pkg/awsutil"
 	"github.com/whatsfordinner/beer-art-bot/pkg/brewerydb"
 )
 
+type getBeersConfig struct {
+	Bucket string
+	Region string
+}
+
 func main() {
+	config, err := loadGetBeersConfig()
+	if err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+
+	log.Printf("creating new AWS session")
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(config.Region),
+	})
+	if err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+
 	log.Printf("fetching beers and styles from BreweryDB")
 	names, styles, err := brewerydb.GetBeerData()
 	if err != nil {
 		log.Fatalf("%s", err.Error())
 	}
 
-	//TOOD(whatsfordinner): DRY. separate into function or make method in brewerydb package
-	log.Printf("converting %d beer names to JSON blob", len(names.BeerData))
-	namesBlob, err := json.Marshal(names)
+	err = brewerydb.WriteBeerOutputToS3(config.Bucket, "beer_names.json", names, sess)
 	if err != nil {
 		log.Fatalf("%s", err.Error())
 	}
 
-	log.Printf("converting %d beer styles to JSON blob", len(styles.BeerData))
-	stylesBlob, err := json.Marshal(styles)
-	if err != nil {
-		log.Fatalf("%s", err.Error())
-	}
-
-	//TODO(whatsfordinner): separate config into struct and own function/init()
-	bucket, exist := os.LookupEnv("UPLOAD_BUCKET")
-	if !exist {
-		log.Fatalf("UPLOAD_BUCKET not set, cannot continue")
-	}
-	region, exist := os.LookupEnv("UPLOAD_REGION")
-	if !exist {
-		log.Fatalf("UPLOAD_REGION not set, cannot continue")
-	}
-
-	log.Printf("creating new AWS session")
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(region),
-	})
-	if err != nil {
-		log.Fatalf("%s", err.Error())
-	}
-	err = awsutil.WriteByteSliceToS3(bucket, "beer_names.json", namesBlob, sess)
-	if err != nil {
-		log.Fatalf("%s", err.Error())
-	}
-	err = awsutil.WriteByteSliceToS3(bucket, "beer_styles.json", stylesBlob, sess)
+	err = brewerydb.WriteBeerOutputToS3(config.Bucket, "beer_styles.json", styles, sess)
 	if err != nil {
 		log.Fatalf("%s", err.Error())
 	}
 
 	log.Printf("finished")
+}
+
+func loadGetBeersConfig() (getBeersConfig, error) {
+	bucket, exist := os.LookupEnv("UPLOAD_BUCKET")
+	if !exist {
+		return getBeersConfig{}, errors.New("UPLOAD_BUCKET not set, cannot continue")
+	}
+	region, exist := os.LookupEnv("UPLOAD_REGION")
+	if !exist {
+		return getBeersConfig{}, errors.New("UPLOAD_REGION not set, cannot continue")
+	}
+
+	return getBeersConfig{
+		Bucket: bucket,
+		Region: region,
+	}, nil
 }
